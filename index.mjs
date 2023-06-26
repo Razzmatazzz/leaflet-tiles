36fe517d7ecb8fec547c5155be0064c6cfa57665
GrantBirki <grantbirki@github.com>
2023-06-26 10:08:13 -0600

    bring the semis back from the dead

--- index.mjs
+++ index.mjs
@@ -1,62 +1,62 @@
-import fs from 'fs/promises'
-import path from 'path'
+import fs from 'fs/promises';
+import path from 'path';
 
-import sharp from 'sharp'
-import ora from 'ora'
-import dotenv from 'dotenv'
+import sharp from 'sharp';
+import ora from 'ora';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const imagePath = process.env.IMAGE_PATH || process.argv[2]
-let inputImage = sharp(imagePath)
+const imagePath = process.env.IMAGE_PATH || process.argv[2];
+let inputImage = sharp(imagePath);
 
-let metadata = inputImage.metadata()
+let metadata = inputImage.metadata();
 
-const extendWidth = metadata.width % 256
-const extendHeight = metadata.height % 256
+const extendWidth = metadata.width % 256;
+const extendHeight = metadata.height % 256;
 if (extendWidth || extendHeight) {
   inputImage = inputImage.extend({
     right: extendWidth,
     bottom: extendHeight,
     background: {r: 1, g: 0, b: 0, alpha: 0}
-  })
+  });
 }
 
 const maxZoom = Math.min(
   process.env.MAX_ZOOM,
   Math.max(metadata.width + extendWidth, metadata.height + extendHeight) / 256 -
     1
-)
-const minZoom = process.env.MIN_ZOOM || 0
+);
+const minZoom = process.env.MIN_ZOOM || 0;
 
 fs.mkdir('output').catch(error => {
   if (!error.code === 'EEXIST') {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 const mapName =
   process.env.MAP_NAME ||
   imagePath.substring(
     imagePath.lastIndexOf(path.sep) + 1,
     imagePath.lastIndexOf('.')
-  )
+  );
 fs.mkdir(`output/${mapName}`).catch(error => {
   if (!error.code === 'EEXIST') {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-let totalTiles = 0
+let totalTiles = 0;
 for (let z = minZoom + 1; z <= maxZoom + 1; z++) {
-  totalTiles += z * z
+  totalTiles += z * z;
 }
 
-const tiles = []
-let completedTiles = 0
-const zoomSpinner = ora({text: `${mapName}`, prefixText: '0.00%'})
+const tiles = [];
+let completedTiles = 0;
+const zoomSpinner = ora({text: `${mapName}`, prefixText: '0.00%'});
 for (let z = minZoom; z <= maxZoom; z++) {
-  zoomSpinner.start(`${mapName} | z ${z}/${maxZoom}`)
+  zoomSpinner.start(`${mapName} | z ${z}/${maxZoom}`);
   const scaledMap = sharp(
     inputImage
       .clone()
@@ -68,18 +68,18 @@ for (let z = minZoom; z <= maxZoom; z++) {
         background: {r: 1, g: 0, b: 0, alpha: 0}
       })
       .toBuffer()
-  )
+  );
   fs.mkdir(`output/${mapName}/${z}`).catch(error => {
     if (!error.code === 'EEXIST') {
-      console.log(error)
+      console.log(error);
     }
-  })
+  });
   for (let x = 0; x <= z; x++) {
     fs.mkdir(`output/${mapName}/${z}/${x}`).catch(error => {
       if (!error.code === 'EEXIST') {
-        console.log(error)
+        console.log(error);
       }
-    })
+    });
     for (let y = 0; y <= z; y++) {
       tiles.push(
         scaledMap
@@ -92,20 +92,20 @@ for (let z = minZoom; z <= maxZoom; z++) {
           })
           .toFile(`output/${mapName}/${z}/${x}/${y}.png`)
           .then(() => {
-            zoomSpinner.suffixText = `| x ${x}/${z} | y ${y}/${z}`
-            completedTiles++
+            zoomSpinner.suffixText = `| x ${x}/${z} | y ${y}/${z}`;
+            completedTiles++;
             zoomSpinner.prefixText = `${(
               Math.round((completedTiles / totalTiles) * 10000) / 100
-            ).toFixed(2)}%`
+            ).toFixed(2)}%`;
           })
-      )
+      );
     }
     if (tiles.length > 100) {
-      Promise.all(tiles)
-      tiles.length = 0
+      Promise.all(tiles);
+      tiles.length = 0;
     }
   }
-  zoomSpinner.suffixText = ''
-  zoomSpinner.prefixText = ''
+  zoomSpinner.suffixText = '';
+  zoomSpinner.prefixText = '';
 }
-zoomSpinner.succeed()
+zoomSpinner.succeed();
