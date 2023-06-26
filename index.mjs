0021acad5a4f3a5106e1692a25fb65f5b51e4a9c
Razzmatazz <35779878+Razzmatazzz@users.noreply.github.com>
2023-06-26 11:12:12 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -10,7 +10,7 @@ dotenv.config();
 const imagePath = process.env.IMAGE_PATH || process.argv[2];
 let inputImage = sharp(imagePath);
 
-let metadata = await inputImage.metadata();
+let metadata = await inputImage.metadata(); // eslint-disable-line
 
 const extendWidth = metadata.width % 256;
 const extendHeight = metadata.height % 256;
